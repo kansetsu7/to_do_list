8b74f02c0fd3410428cafe7a7efe4a9730309896
Kansetsu Kuo <kansetsu4@gmail.com>
2017-11-30 11:37:58 +0800

    change DB column "completed"'s default to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129055515) do
+ActiveRecord::Schema.define(version: 20171130032526) do
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.date "due_date"
     t.text "note"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
