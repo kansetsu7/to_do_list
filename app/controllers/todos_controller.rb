14d0a5e55dff7e2b18996a7be023f634bfc03c85
Kansetsu Kuo <kansetsu4@gmail.com>
2017-12-01 14:48:06 +0800

    Add function: destroy.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,22 +1,35 @@
 class TodosController < ApplicationController
+  # before_action :set_todo, only => [:destroy]
   def index
     @todos = Todo.all
     @todo = Todo.new
   end
 
+
   def create
     @todo = Todo.new(todo_params)
-    @todos = Todo.all
     if @todo.save
       flash[:notice] = "Successfully add mission \"#{@todo.name}\""
       redirect_to todos_url
     else
+      @todos = Todo.all
       flash[:alert] = "gg.."
-      render :action => :index
+      render :index
     end
   end
 
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+
+    redirect_to todos_url
+  end
+
   private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
 
   def todo_params
     params.require(:todo).permit(:name, :due_date, :note)    
